50814c5660ab020183258c32897b81b961e1b342
yamid-dev <yhrodriguez1@hotmail.com>
2024-06-29 23:35:41 -0500

    Función check ready lista

--- frontend/src/assets/components/TransactionList.jsx
+++ frontend/src/assets/components/TransactionList.jsx
@@ -1,4 +1,4 @@
-import {React,useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const TransactionList = ({
@@ -13,51 +13,42 @@ const TransactionList = ({
 }) => {
   
   const [checkedItems, setCheckedItems] = useState({});
-  
-  const checkBuysOrDebts = async (event,id,type) => {
+
+  // Función para inicializar checkedItems basado en transactions
+  useEffect(() => {
+    const initialCheckedItems = {};
+    transactions.forEach(transaction => {
+      initialCheckedItems[transaction.id] = transaction.ready === 1;
+    });
+    setCheckedItems(initialCheckedItems);
+  }, [transactions]);
+
+  const checkBuysOrDebts = async (event, id, type) => {
     const isChecked = event.target.checked;
     setCheckedItems(prevCheckedItems => ({
       ...prevCheckedItems,
       [id]: isChecked
     }));
-    try{
 
-       // Lógica que quieres ejecutar cuando el checkbox cambie
-        if (type==="Buys" && isChecked) {
-            const state = 1;
-            console.log(`Compra #${id} realizada!`);
-            const response = await axios.post(`${HTTP_ENDPOINT}/ready/${type}/${id}/${state}`);
-            if (response.data.transaction){
-               alert(`Compra #${id} realizada!`) 
-            }
-        } else if (type==="Debts" && isChecked){
-            const state = 1;
-            console.log(`Deuda #${id} saldada!`);
-            const response = await axios.post(`${HTTP_ENDPOINT}/ready/${type}/${id}/${state}`);
-            if (response.data.transaction){
-               alert(`Deuda #${id} saldada con éxito!`) 
-            }
-        } else if(type==="Debts" && !isChecked){
-            const state = 0;
-            console.log(`El pago de la deuda #${id} se canceló`)
-            const response = await axios.post(`${HTTP_ENDPOINT}/ready/${type}/${id}/${state}`);
-            if (response.data.transaction){
-               alert(`Pago de la deuda #${id} cancelada con éxito!`) 
-            }
-        } else{
-            const state = 0;
-            console.log(`La compra #${id} se canceló`)
-            const response = await axios.post(`${HTTP_ENDPOINT}/ready/${type}/${id}/${state}`);
-            if (response.data.transaction){
-               alert(`Deuda #${id} saldada con éxito!`) 
-            }
+    try {
+      const state = isChecked ? 1 : 0;
+      const response = await axios.post(`${HTTP_ENDPOINT}/ready/${type}/${id}/${state}`);
+      if (response.data.transaction) {
+        const updatedTransactions = transactions.map(transaction =>
+          transaction.id === id ? { ...transaction, ready: state } : transaction
+        );
+        setTransactions(updatedTransactions);
+        if (isChecked) {
+          alert(type === 'Buys' ? `Compra #${id} realizada!` : `Deuda #${id} saldada con éxito!`);
+        } else {
+          alert(type === 'Buys' ? `La compra #${id} se canceló` : `Pago de la deuda #${id} cancelada con éxito!`);
         }
-    } catch(error){
-        console.log(error)
+      }
+    } catch (error) {
+      console.error('Error updating transaction state:', error);
+      alert('Error actualizando el estado de la transacción. Por favor, intenta de nuevo.');
     }
-
-  }
-
+  };
 
   const deleteTransaction = async (id) => {
     try {
@@ -113,7 +104,11 @@ const TransactionList = ({
                 <h3 style={{ color: 'blue' }}>Compra</h3>
                 <p>{transaction.description}</p>
                 <h6>{formatPrice(transaction.price, exchangeRate)[0]}</h6>
-                <h6 style={{ color: getColor(transaction.type)[1] }}>Listo: <input  className="checks-buys" type="checkbox" id={`check-${transaction.id}`} name="check_buy" onChange={(event)=>{checkBuysOrDebts(event, transaction.id,transaction.type)}}></input></h6>
+                <h6 style={{ color: getColor(transaction.type)[1] }}>
+                  Listo: <input className="checks-buys" type="checkbox" id={`check-${transaction.id}`} name="check_buy"
+                                onChange={(event) => { checkBuysOrDebts(event, transaction.id, transaction.type) }}
+                                checked={checkedItems[transaction.id] || false}></input>
+                </h6>
               </div>
             )}
             {transaction.type === 'Debts' && (
@@ -121,22 +116,18 @@ const TransactionList = ({
                 <h3 style={{ color: getColor(transaction.type)[1] }}>Deuda</h3>
                 <p>{transaction.description}</p>
                 <h6>{formatPrice(transaction.price, exchangeRate)[0]}</h6>
-                <h6 style={{ color: getColor(transaction.type)[1] }}>Listo: <input className="checks-debts" type="checkbox" id={`check-${transaction.id}`} name="check_debt" onChange={(event)=>{checkBuysOrDebts(event, transaction.id,transaction.type)}}></input></h6>
+                <h6 style={{ color: getColor(transaction.type)[1] }}>
+                  Listo: <input className="checks-debts" type="checkbox" id={`check-${transaction.id}`} name="check_debt"
+                                onChange={(event) => { checkBuysOrDebts(event, transaction.id, transaction.type) }}
+                                checked={checkedItems[transaction.id] || false}></input>
+                </h6>
               </div>
             )}
             <div style={{ display: 'flex', justifyContent: 'left', gap: '3%' }}>
-              <button
-                style={{ background: 'grey', border: 'none', padding: '1%' }}
-                onClick={() => deleteTransaction(transaction.id)}
-              >
-                Borrar
-              </button>
-              <button
-                style={{ background: getColor(transaction.type)[1], border: 'none', padding: '1%' }}
-                onClick={() => onSeeMore(transaction)}
-              >
-                Ver más
-              </button>
+              <button style={{ background: 'grey', border: 'none', padding: '1%' }}
+                      onClick={() => deleteTransaction(transaction.id)}>Borrar</button>
+              <button style={{ background: getColor(transaction.type)[1], border: 'none', padding: '1%' }}
+                      onClick={() => onSeeMore(transaction)}>Ver más</button>
             </div>
           </div>
         ))}
